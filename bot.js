04899e0aefc73626a427ab10a7385d88ff91ded2
GwenBebe <58706911+GwenBebe@users.noreply.github.com>
2020-07-13 21:19:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const BOT_TOKEN = "NjcyNTQ4NDM3MzQ2MjIyMTEw.XlqgFw.kmBSFsir4CYtj26HHuI7UeJySjc";
+const BOT_TOKEN = "";
 
 client.login(BOT_TOKEN);
 
